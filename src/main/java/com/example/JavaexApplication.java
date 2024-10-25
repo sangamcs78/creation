e4539c5ac2f29d84446feb70bdac0c152c97158f
sangamcs78 <sangam0365207@gmail.com>
2024-10-25 11:08:05 +0530

    example

---
+++ src/main/java/com/example/JavaexApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaexApplication.class, args);
+	}
+
+}
